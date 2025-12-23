9a3c17244882f6355ba5632dbea7874a8be15248
casnix <casnix@github.com>
2025-12-23 09:05:14 -0600

    File reorg

---
+++ singlewire-fusion-py/FusionAPI/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='FusionAPI',
+    version='0.1dev',
+    packages=['fusionapi',],
+    license='The MIT License',
+    long_description=open('README.txt').read(),
+)
